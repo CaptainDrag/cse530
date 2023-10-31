1740179e8c2777ca802c85ae382f5df9aa9d6674
CaptainDrag <112833837+CaptainDrag@users.noreply.github.com>
2023-10-31 14:23:39 -0400

    Add files via upload

---
+++ result/run0.sh
@@ -0,0 +1,2 @@
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=large --redirect-stdout --redirect-stderr /home/grads/ipm5198/hw4/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=large --binfile=/home/grads/ipm5198/hw4/Quicksort  --l1i_size "4kB" --l1d_size "4kB" --l2_size "32kB"
+/home/other/CSE530-FA2022/gem5/build_a4/build/X86/gem5.opt --outdir=small --redirect-stdout --redirect-stderr /home/grads/ipm5198/hw4/cse530_a4/cse530_a4_sys_config.py --cse530-core-config=small --binfile=/home/grads/ipm5198/hw4/Quicksort  --l1i_size "4kB" --l1d_size "4kB" --l2_size "32kB"
